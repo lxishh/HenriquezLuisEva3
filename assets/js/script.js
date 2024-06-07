411c9ae34ce570dd561b172e165b2408e5c830ab
lxishh <henrix913@gmail.com>
2024-06-07 10:11:30 -0400

    botones fuente/contraste (falta)
    se agregó la estructura de los botones, pero falta realizar el toggle entre clases

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 console.log("Test")
 
-// window.addEventListener("load", ()=>{
-//     window.addEventListener("click", contraste)
-// })
+window.addEventListener("load", ()=>{
+    document.getElementById("btnContraste").addEventListener("click", contraste);
+    // document.getElementById("btnFuente").addEventListener("click", fuente);
+})
+
+const contraste = ()=>{
+    let eContraste = document.body.className
+    console.log(eContraste)
+}
+
+
 
