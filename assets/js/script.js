a663887e4bb1b905729b41493d9b5a4d85319ffb
lxishh <henrix913@gmail.com>
2024-06-08 19:43:09 -0400

    INCLUSIVIDAD
    se agregaron los botones de contraste y fuente (en el nav)
    
    se crearon las clases en el css
    
    se crearon las funciones en javascript:
    
    -funcion contraste; fuente y luego
    se agregaron a la funcion flecha del load (con click cada una)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,22 @@
 console.log("Test")
 
+// Se agregar un LISTENER el cual espera a que CARGUE la pagina para posteriormente ejecutar lo que encuentre dentro de las llaves de la funcion flecha ( ()=> )
+    //se SELECIONA el BOTON de CONTRASTE a traves de su ID y se le asigna un listener el cual al hacer click se ejecuta la funcion "contraste"
 window.addEventListener("load", ()=>{
     document.getElementById("btnContraste").addEventListener("click", contraste);
-    // document.getElementById("btnFuente").addEventListener("click", fuente);
+    document.getElementById("btnFuente").addEventListener("click", fuente);
 })
 
-const contraste = ()=>{
-    let eContraste = document.body.className
-    console.log(eContraste)
+// se crea la FUNCION "contraste"
+// se declara la variable "CUERPO" en la cual se obtiene el body a traves de su ID.
+//va alterando la clase del BODY entre "modo-claro" y "modo-oscuro".
+function contraste(){
+    var cuerpo = document.getElementById("idBody");
+    cuerpo.classList.toggle("modo-claro")
+    cuerpo.classList.toggle("modo-oscuro")
 }
 
 
-
+function fuente(){
+    document.body.classList.toggle("fontsize");
+}
