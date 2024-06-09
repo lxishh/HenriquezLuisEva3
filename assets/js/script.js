ee7fb71ea6b19b5593188f2059aa8377297efdff
lxishh <henrix913@gmail.com>
2024-06-08 21:07:26 -0400

    VALIDACIONES
    se creo la clase noDisplay en css
    
    en el script se agrego validar al listener de load
    
    se crearon las funciones de validar (3)
    una para campos normales de texto
    otra para longitud
    y la ultima para checkbox
    
    en el index (html) se agregaron parrafos con la clase noDisplay para mostrarle al usuario sus errores.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,11 @@ console.log("Test")
 window.addEventListener("load", ()=>{
     document.getElementById("btnContraste").addEventListener("click", contraste);
     document.getElementById("btnFuente").addEventListener("click", fuente);
+    document.getElementById("btnRegistrar").addEventListener("click", validar);
 })
+
+
+// A continuacion se aplica la inclusividad:
 
 // se crea la FUNCION "contraste"
 // se declara la variable "CUERPO" en la cual se obtiene el body a traves de su ID.
@@ -16,7 +20,72 @@ function contraste(){
     cuerpo.classList.toggle("modo-oscuro")
 }
 
-
+//simplemente se le agrega "fontsize" a la clase del BODY.
 function fuente(){
     document.body.classList.toggle("fontsize");
+}
+
+
+//validacion de errores y alerta de errores
+
+//esta funcion sirve para juntar las distintas funciones de validacion que se creen
+function validar(){
+    validarVacio('nombre')
+    validarVacio('correo')
+    validarVacio('contraseña')
+    validarLongitud('telefono', 9)
+    validarTerminos('terminos')
+
+}
+
+//se guarda el elemento/campo en la variable eNombre
+//se guarda el valor (lo que haya escrito) de ese campo, en la variable vNombre
+// se comprueba si el campo esta vacio, comparandolo con un string vacio. 
+//  ".trim()" elimina los espacios en blanco.
+// en caso de que este vacio:
+// se le cambia la propiedad del display a la clase 'NoDsiplay" y pasa a ser block,
+// por lo tanto nos muestra un parrafo especificandonos el error.
+function validarVacio(Campo){
+    let eNombre = document.getElementById(Campo);
+    let vNombre = eNombre.value;
+
+    if(vNombre.trim()==""){
+        eNombre.style.borderColor = "red";
+        let pNombre = document.getElementById("p"+Campo);
+        pNombre.style.display = "block";
+    }else{
+        eNombre.style.borderColor = "green";
+        let pNombre = document.getElementById("p"+Campo);
+        pNombre.style.display = "none";
+    }
+}
+
+
+//esta funcion guarda el valor(input) del campo seleccionado
+// luego con length obtenemos la longitud o el numero de caracteres que tiene ese valor
+// ese ese valor se compara, si es distinto al largo que le especificamos, se ejecutara lo que esta dentro del if
+// el if lo que hace es poner los bordes de color y mostrar/quitar el parrafo que indica el error. 
+function validarLongitud(Campo, largo){
+    let elemento = document.getElementById(Campo);
+    let valor = elemento.value
+
+    if (valor.length != largo){
+        elemento.style.borderColor="red";
+        let pNombre = document.getElementById("p"+Campo);
+        pNombre.style.display = "block";
+    }else{
+        elemento.style.borderColor = "green";
+        let pNombre = document.getElementById("p"+Campo);
+        pNombre.style.display = "none";
+    }
+}
+
+//se verifica que el checkbox de los terminos y condicionesse encuentre marcado
+// de no ser asi, muestra una alerta por pantalla diciendo que debe aceptarlos.
+function validarTerminos(Campo){
+    let eNombre = document.getElementById(Campo);
+
+    if(!eNombre.checked){
+        alert('Debe aceptar los terminos y condiciones.')
+    }
 }
